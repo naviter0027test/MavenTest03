ca53d308693df323f225de7c2e25b0f8bed405cd
naviter0027test <naviter0027test@gmail.com>
2021-10-14 14:26:07 +0800

    管理:密碼更改. 設定session 120分鐘時限

--- src/main/java/tst/admin/mod/AdminModel.java
+++ src/main/java/tst/admin/mod/AdminModel.java
@@ -2,9 +2,11 @@ package tst.admin.mod;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.HashMap;
 
 public class AdminModel {
@@ -53,5 +55,43 @@ public class AdminModel {
 		}
 
 		return adminData;
+	}
+	
+	public HashMap<String, String> getById(String id) throws SQLException {
+		if(conn == null)
+			throw new SQLException("not connect");
+		
+		stmt = conn.createStatement();
+
+		String sql = "select id, account, password from admin where id = '"+ id+ "'";
+		ResultSet result = stmt.executeQuery(sql);
+
+		HashMap<String, String> adminData = new HashMap<String, String>();
+    	if(result != null) {
+			while (result.next()) {
+				String tmpId = Integer.toString(result.getInt(1));
+				String tmpAcc = result.getString(2);
+				String tmpPass = result.getString(3);
+				adminData.put("id", tmpId);
+				adminData.put("account", tmpAcc);
+				adminData.put("password", tmpPass);
+			}
+		}
+
+		return adminData;
+	}
+	
+	public void passwordUpdate(String id, String passwordNew) throws SQLException {
+		if(conn == null)
+			throw new SQLException("not connect");
+		Timestamp createdDate = new Timestamp(System.currentTimeMillis());
+
+		stmt = conn.createStatement();
+		String sql = "update admin set password = ? where id = ?";
+		PreparedStatement prepare = conn.prepareStatement(sql);
+		prepare.setString(1, passwordNew);
+		prepare.setString(2, id);
+		
+		prepare.executeUpdate();
 	}
 }
--- src/main/java/tst/admin/page/AdminPass.java
+++ src/main/java/tst/admin/page/AdminPass.java
@@ -2,22 +2,64 @@ package tst.admin.page;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import tst.admin.mod.AdminModel;
 
 public class AdminPass extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = res.getWriter();
+		HttpSession session = req.getSession();
+		if(session.getAttribute("adminId") == null) {
+			res.sendRedirect("admin-login");
+			return;
+		}
 		req.getRequestDispatcher("admin/password.jsp").forward(req, res);        
 	}
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = res.getWriter();
-		out.println("admin password doPost");        
+		HttpSession session = req.getSession();
+		if(session.getAttribute("adminId") == null) {
+			res.sendRedirect("admin-login");
+			return;
+		}
+		AdminModel admin = new AdminModel();
+		String adminId = (String) session.getAttribute("adminId");
+		String passwordOld = req.getParameter("passwordOld");
+		String password = req.getParameter("password");
+		String passwordConfirm = req.getParameter("passwordConfirm");
+		if(password.equals(passwordConfirm) == false) {
+			out.println("<meta http-equiv='refresh' content='3; url=admin-pass'>");
+			out.println("密碼與確認密碼輸入須一樣<br />");
+		}
+		try {
+			admin.connectMariaDB();
+			HashMap<String, String> adminData = admin.getById(adminId);
+			String tmpPass = adminData.get("password");
+			if(tmpPass.equals(passwordOld) == false) {
+				out.println("<meta http-equiv='refresh' content='3; url=admin-pass'>");
+				out.println("舊密碼輸入錯誤<br />");				
+			}
+
+			admin.passwordUpdate(adminId, password);
+			admin.closeMariaDB();
+			out.println("<meta http-equiv='refresh' content='3; url=admin-pass'>");
+		} catch(SQLException | ClassNotFoundException e) {
+			out.println(e.getMessage());
+		}
+//		out.println("passwordOld:"+ passwordOld+ "<br />");
+//		out.println("password:"+ password+"<br />");
+//		out.println("passwordConfirm:"+ passwordConfirm+ "<br />");
+		out.println("密碼更新完成，等待跳轉...<br />");
 	}
 }
